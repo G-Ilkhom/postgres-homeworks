a1642c7583e33801f305c29f7df8d2fda0f6e2c0
ILKHOM <gaffarovilkhom@inbox.ru>
2024-02-18 22:54:43 +0300

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers(
+	customer_id serial PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE orders(
+	order_id serial PRIMARY KEY,
+	customer_id int REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(100)
+);
